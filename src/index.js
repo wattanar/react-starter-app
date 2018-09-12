2fe6508c833c523bd25c2e8179c717d523d74f24
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-09-12 10:36:40 +0700

    add bootstrap css here

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import './styles/bootstrap/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
