cfaf3d8cd0463cd272fb063b7c441e8def677778
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-09-12 10:36:58 +0700

    use fragment

--- src/components/Example.js
+++ src/components/Example.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Example extends React.Component {
   render() {
-    return <div>{this.props.message}</div>;
+    return <React.Fragment>{this.props.message}</React.Fragment>;
   }
 }
