e2be38c4b1e9bfd2d0d5545e4ed735e1f188f36a
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-09-22 12:45:01 +0700

    merge container and component

---
+++ src/components/Counter/CounterComponent.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { inc, dec, current } from './CounterAction';
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    this.props.dispatch(current());
+  }
+
+  inc() {
+    this.props.dispatch(inc(this.props.counterState.counter));
+  }
+
+  dec() {
+    this.props.dispatch(dec(this.props.counterState.counter));
+  }
+
+  render() {
+    return (
+      <div className="text-center">
+        <h1>Counter</h1>
+        <h3>{this.props.counterState.counter}</h3>
+        <div>
+          <button
+            className="btn btn-danger"
+            onClick={this.dec.bind(this)}
+            style={{ marginRight: '10px' }}
+          >
+            - 1
+          </button>
+          <button className="btn btn-success" onClick={this.inc.bind(this)}>
+            + 1
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    counterState: state.CounterReducer
+  };
+};
+
+export default connect(mapStateToProps)(Counter);
