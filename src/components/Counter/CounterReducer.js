965c9ad0af6e344eb0afa857dfa6f42f705b708f
wattanar <iamwattanas@gmail.com>
2018-09-23 23:03:44 +0700

    add set current counter

--- src/components/Counter/CounterReducer.js
+++ src/components/Counter/CounterReducer.js
@@ -4,11 +4,13 @@ const intitialState = {
 
 export const CounterReducer = (state = intitialState, action) => {
   switch (action.type) {
-    case 'COUNTER_CURRENT':
+    case "COUNTER_CURRENT":
       return state;
-    case 'COUNTER_INC':
+    case "COUNTER_INC":
       return { ...state, counter: action.payload };
-    case 'COUNTER_DEC':
+    case "COUNTER_DEC":
+      return { ...state, counter: action.payload };
+    case "SET_CURRENT_COUNTER":
       return { ...state, counter: action.payload };
     default:
       return state;
