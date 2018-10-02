0669aa4dad6831fbb4c8ae7cbd704fae7b67329f
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-10-02 08:53:38 +0700

    add counter page

---
+++ src/pages/Counter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CounterComponent from '../components/Counter/CounterComponent';
+
+class Counter extends React.Component {
+  render() {
+    return <CounterComponent />;
+  }
+}
+
+export default Counter;
