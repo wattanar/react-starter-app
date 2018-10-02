9fc78b223b59199bef55625c28b9317cc2183fcc
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-10-02 08:53:13 +0700

    check from Type.js

--- src/components/Counter/CounterReducer.js
+++ src/components/Counter/CounterReducer.js
@@ -1,16 +1,23 @@
+import {
+  COUNTER_CURRENT,
+  COUNTER_INC,
+  COUNTER_DEC,
+  SET_CURRENT_COUNTER
+} from "../../Types";
+
 const intitialState = {
   counter: 0
 };
 
 export const CounterReducer = (state = intitialState, action) => {
   switch (action.type) {
-    case "COUNTER_CURRENT":
+    case COUNTER_CURRENT:
       return state;
-    case "COUNTER_INC":
+    case COUNTER_INC:
       return { ...state, counter: action.payload };
-    case "COUNTER_DEC":
+    case COUNTER_DEC:
       return { ...state, counter: action.payload };
-    case "SET_CURRENT_COUNTER":
+    case SET_CURRENT_COUNTER:
       return { ...state, counter: action.payload };
     default:
       return state;
