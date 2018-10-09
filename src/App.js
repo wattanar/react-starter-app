3d59621ed1053de600047924e3fff68257746990
wattanar <iamwattanas@gmail.com>
2018-10-09 21:57:54 +0700

    import Header and Main component instead

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import NotFound from './components/Common/NotFound';
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default class App extends React.Component {
   state = {
-    projectName: 'React Starter App',
+    projectName: "React Starter App",
     isOpen: false
   };
 
@@ -18,29 +16,13 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <div className="app-container">
-            <h1>{this.state.projectName}</h1>
-            <ul className="app-nav">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/counter">Simple Counter</Link>
-              </li>
-              <li>
-                <Link to="/404">Not found</Link>
-              </li>
-            </ul>
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/counter" component={Counter} />
-              <Route component={NotFound} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <React.Fragment>
+        <div className="app-container">
+          <h1>{this.state.projectName}</h1>
+          <Header />
+          <Main />
+        </div>
+      </React.Fragment>
     );
   }
 }
