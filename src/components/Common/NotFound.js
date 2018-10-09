94f6bf2a5b09215e846efb219a314ad6431b9614
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-10-09 11:17:59 +0700

    move to common

---
+++ src/components/Common/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NotFound.css';
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <div className="block-notfound">
+        <h1>404 - Page not found!</h1>
+        <div>The page you looking for is not exists.</div>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
