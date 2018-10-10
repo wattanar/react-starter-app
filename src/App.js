11be1eded04c4a6e9db9944d7404a265fef8e1d0
wattanar <iamwattanas@gmail.com>
2018-10-10 23:53:16 +0700

    split to component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="app-container">
-          <h1>React Starter App</h1>
-          <Header />
-          <Main />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Main />
+    </React.Fragment>
+  );
+};
+
+export default App;
