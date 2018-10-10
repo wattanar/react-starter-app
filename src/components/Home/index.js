6b1660347ff51be5b43a4a437e2d4ec94d5b1bc2
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-10-10 15:54:29 +0700

    update to container component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,29 +1,5 @@
-import React from 'react';
 import { connect } from 'react-redux';
-import { setCurrentCounter } from '../../services/CounterService';
-
-class Home extends React.Component {
-  setCurrentCounter() {
-    setCurrentCounter();
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Home Page</h1>
-        <p>Current counter is : {this.props.counterState.counter}</p>
-        <div>
-          <button
-            onClick={this.setCurrentCounter.bind(this)}
-            className="btn btn-primary"
-          >
-            Set Counter = 100
-          </button>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+import Home from './HomeComponent';
 
 const mapStateToProps = state => {
   return {
