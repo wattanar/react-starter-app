b41b2740d235a103b0fac088bbdf8402dbb02251
wattanar <iamwattanas@gmail.com>
2018-10-10 23:54:09 +0700

    use bootstrap

--- src/components/Counter/CounterComponent.js
+++ src/components/Counter/CounterComponent.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import './Counter.css';
+import React from "react";
+import "./Counter.css";
 
 const Counter = props => {
   return (
     <React.Fragment>
-      <h1>Counter</h1>
-      <h3>{props.counterState.counter}</h3>
-      <div>
-        <button
-          className="counter-button"
-          onClick={props.decreaseCounter.bind(this, props.counterState.counter)}
-        >
-          - 1
-        </button>
-        <button
-          className="counter-button"
-          onClick={props.increaseCounter.bind(this, props.counterState.counter)}
-        >
-          + 1
-        </button>
+      <div className="text-center header-top-space">
+        <h1>Counter</h1>
+        <h3>{props.counterState.counter}</h3>
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={props.decreaseCounter.bind(
+              this,
+              props.counterState.counter
+            )}>
+            - 1
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={props.increaseCounter.bind(
+              this,
+              props.counterState.counter
+            )}>
+            + 1
+          </button>
+        </div>
       </div>
     </React.Fragment>
   );
