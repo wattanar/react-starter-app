4e3a81fa7bf4077c6412cbf0287d8065c798dbe7
wattanar <iamwattanas@gmail.com>
2018-10-10 23:55:24 +0700

    add component

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { _setCurrentCounter } from '../../services/CounterService';
+import React from "react";
+import { _setCurrentCounter } from "../../services/CounterService";
 
 const Home = props => {
   return (
     <React.Fragment>
-      <h1>Home Page</h1>
-      <p>Current Counter is : {props.counterState.counter}</p>
-      <div>
-        <button
-          onClick={_setCurrentCounter.bind(this)}
-          className="btn btn-primary"
-        >
-          Set Counter = 100
-        </button>
+      <div className="text-center header-top-space">
+        <h1>Home Page</h1>
+        <p>Current Counter is : {props.counterState.counter}</p>
+        <div className="btn-group">
+          <button
+            onClick={_setCurrentCounter.bind(this, 0)}
+            className="btn btn-default">
+            Set Counter = 0
+          </button>
+          <button
+            onClick={_setCurrentCounter.bind(this, 100)}
+            className="btn btn-primary">
+            Set Counter = 100
+          </button>
+        </div>
       </div>
     </React.Fragment>
   );
