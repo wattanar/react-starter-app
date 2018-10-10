d7c7f74e71d13fc7bb43f8d17f976734082e62be
wattanar <iamwattanas@gmail.com>
2018-10-10 23:55:34 +0700

    add component

---
+++ src/components/Main/MainComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "../Home";
+import Counter from "../Counter";
+import NotFound from "../Common/NotFound";
+
+const MainComponent = () => {
+  return (
+    <div className="container">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/counter" component={Counter} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  );
+};
+
+export default MainComponent;
