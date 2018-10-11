523397e2bfc8904afd1cc5d59d546405c75490e5
wattanar <iamwattanas@gmail.com>
2018-10-11 23:32:41 +0700

    set variable name

--- generator/index.js
+++ generator/index.js
@@ -1,14 +1,15 @@
-const pascalCase = require('pascal-case');
-const commandLineArgs = require('command-line-args');
-const signale = require('signale');
-const fs = require('fs-extra');
+const pascalCase = require("pascal-case");
+const commandLineArgs = require("command-line-args");
+const signale = require("signale");
+const fs = require("fs-extra");
+const replace = require("replace-in-file");
 
-let componentName = '';
+let componentName = "";
 
 const optionDefinitions = [
   {
-    name: 'name',
-    alias: 'n',
+    name: "name",
+    alias: "n",
     type: String,
     multiple: true
   }
@@ -17,43 +18,60 @@ const optionDefinitions = [
 const args = commandLineArgs(optionDefinitions, { partial: true });
 
 if (args.name.length === 0) {
-  signale.error('No component name!');
+  signale.error("No component name!");
 }
 
 args.name.map(v => {
   componentName += pascalCase(v);
 });
 
-console.log(componentName);
-
 async function copyTemplate() {
   try {
     let a = await fs.copy(
-      './generator/Template/index.js',
+      "./generator/Template/index.js",
       `./src/components/${componentName}/index.js`
     );
 
     await fs.copy(
-      './generator/Template/TemplateComponent.js',
+      "./generator/Template/TemplateComponent.js",
       `./src/components/${componentName}/${componentName}Component.js`
     );
 
     await fs.copy(
-      './generator/Template/TemplateType.js',
+      "./generator/Template/TemplateType.js",
       `./src/components/${componentName}/${componentName}Type.js`
     );
 
     await fs.copy(
-      './generator/Template/TemplateAction.js',
+      "./generator/Template/TemplateAction.js",
       `./src/components/${componentName}/${componentName}Action.js`
     );
 
     await fs.copy(
-      './generator/Template/TemplateReducer.js',
+      "./generator/Template/TemplateReducer.js",
       `./src/components/${componentName}/${componentName}Reducer.js`
     );
 
-    signale.success('generate component success!');
+    signale.success("Generated component completed!");
+
+    const replaceOptions = {
+      files: [
+        `./src/components/${componentName}/index.js`,
+        `./src/components/${componentName}/${componentName}Component.js`,
+        `./src/components/${componentName}/${componentName}Type.js`,
+        `./src/components/${componentName}/${componentName}Action.js`,
+        `./src/components/${componentName}/${componentName}Reducer.js`
+      ],
+      from: /Template/g,
+      to: `${componentName}`
+    };
+
+    replace(replaceOptions, (error, changes) => {
+      if (error) {
+        return signale.error("Error occurred:", error);
+      }
+      signale.success("Modified files completed!");
+    });
   } catch (err) {
     signale.error(err);
   }
