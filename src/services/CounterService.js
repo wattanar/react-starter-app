b66e87ba5ccef905d98309bff0f7950e3be8b026
wattanar <iamwattanas@gmail.com>
2018-10-11 23:52:57 +0700

    removed

--- src/services/CounterService.js
+++
@@ -1,6 +0,0 @@
-import Store from "../Store";
-import { setCurrentCounter } from "../components/Counter/CounterAction";
-
-export const _setCurrentCounter = number => {
-  Store.dispatch(setCurrentCounter(number));
-};
