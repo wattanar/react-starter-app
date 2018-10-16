78c8b8249b04b3863c2b6c2e2555c4de3e508f79
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-10-16 11:20:00 +0700

    add sample map dispatch

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,16 @@
 import { connect } from 'react-redux';
 import HomeComponent from './HomeComponent';
+// import { actionName } from './TemplateAction'
 
 // const mapStateToProps = state => {
 //   return {
-//     HomeState: state.HomeReducer
+//     TemplateState: state.TemplateReducer
 //   };
 // };
 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     actionName: () => dispatch(actionName()),
+//   };
+// };
 export default connect()(HomeComponent);
