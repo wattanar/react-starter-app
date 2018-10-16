25de4684ad50c6c2e806742003fe8f5755aef837
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-10-16 11:18:38 +0700

    add sample map dispatch

--- generator/Template/index.js
+++ generator/Template/index.js
@@ -1,9 +1,16 @@
 import { connect } from 'react-redux';
 import TemplateComponent from './TemplateComponent';
+// import { actionName } from './TemplateAction'
 
 // const mapStateToProps = state => {
 //   return {
 //     TemplateState: state.TemplateReducer
+//   };
+// };
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     actionName: () => dispatch(actionName()),
 //   };
 // };
 
