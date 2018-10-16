0ab498c26615914b06390a7c2d780c3c022a3b93
Wattana Ruengmucha <iamwattanas@gmail.com>
2018-10-16 11:19:44 +0700

    update className

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -4,7 +4,7 @@ class HomeComponent extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<h1 className="app-banner">Hello, World!</h1>
+				<h1 className="mt-50">Hello, World!</h1>
 			</React.Fragment>
 		);
 	}
