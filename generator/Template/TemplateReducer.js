34f203e78f88e2460bbd3c5fbc2ba7e782f6a83b
wattanar <iamwattanas@gmail.com>
2018-10-21 18:05:38 +0700

    set default state

--- generator/Template/TemplateReducer.js
+++ generator/Template/TemplateReducer.js
@@ -1,13 +1,13 @@
 import { ACTION_NAME } from "./TemplateType";
 
 const intitialState = {
-  app_name: ""
+  hello: "Hello, World!"
 };
 
 export const TemplateReducer = (state = intitialState, action) => {
   switch (action.type) {
     case ACTION_NAME:
-      return { ...state, app_name: action.payload };
+      return { ...state, hello: action.payload };
     default:
       return state;
   }
