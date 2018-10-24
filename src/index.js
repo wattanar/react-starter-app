f834383a6c630f7e67630d50b78b30c3234a5aae
wattanar <iamwattanas@gmail.com>
2018-10-24 22:18:07 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import { Provider } from "react-redux";
 import Store from "./Store";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <Provider store={Store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <Provider store={Store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootElement
+  );
+} else {
+  render(
+    <Provider store={Store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootElement
+  );
+}
+
 registerServiceWorker();
